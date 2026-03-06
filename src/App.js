7014c1c2323da27c43fe05541b9268ecabc76eb9
tobbadiru <badirurazaq3@gmail.com>
2026-03-06 17:12:27 +0100

    Navigation and side bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/layout/Navbar/Navbar";
+import Sidebar from "./components/layout/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Navbar />
+
+      <div style={{ display: "flex" }}>
+
+        <Sidebar />
+
+        <div style={{ padding: "20px" }}>
+          <h1>Dashboard</h1>
+        </div>
+
+      </div>
+
     </div>
   );
 }
