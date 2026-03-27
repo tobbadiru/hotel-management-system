29ff03aa5da12724bb58bf3c08cb9bcf1f808f67
tobbadiru <badirurazaq3@gmail.com>
2026-03-27 15:38:30 +0100

    Some more codes

--- src/components/authentication/AddUserForm/AddUserForm.js
+++ src/components/authentication/AddUserForm/AddUserForm.js
@@ -1,1 +1,40 @@
 import "./AddUserForm.css";
+
+function AddUserForm() {
+  return (
+    <div className="add-user-container">
+
+      <div className="add-user-box">
+
+        <h2 className="add-user-title">Add New User</h2>
+
+        <input
+          type="text"
+          placeholder="Username"
+          className="add-user-input"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="add-user-input"
+        />
+
+        <select className="add-user-select">
+          <option>Select Role</option>
+          <option>Admin</option>
+          <option>Receptionist</option>
+          <option>Housekeeping</option>
+        </select>
+
+        <button className="add-user-button">
+          Create User
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default AddUserForm;
